7a5fdb2c9c23968a41b259edc2030c6016e75e75
Malarout <namrata.malarout@jpl.nasa.gov>
2017-11-06 09:08:08 -0800

    malarout: Fixed syntax issues

--- lambda/lambda_function-asf_notification.py
+++ lambda/lambda_function-asf_notification.py
@@ -1,28 +1,21 @@
+#!/usr/bin/env python
 from __future__ import print_function
 
-import json, requests, urllib2, ssl
-from string import Template
+'''
+This lambda function submits a job via MOZART API
+to update ES doc, for book keeping purposes.
+When an SNS message is recieved from ASF reporting
+successful receipt and ingestion of product delivered by our system,
+we want to capture this acknowledgement by stamping the product
+with delivery and ingestion time.
+'''
 
-grq_url = "http://datasets.grfn.hysds.net:9200"
+import json
+import  requests
 
-print('Loading function')
+print 'Loading function'
 
-authorized_ips = """
-35.200.100.101 # My VPN endpoint
-37.192.125.17  # Build agent 1
-37.192.125.18  # Build agent 2
-37.192.125.19  # Build agent 3
-38.192.125.20  # Build agent 4
-"""
 MOZART_URL = 'https://100.64.134.67'
-target_server = "http://jobs.grfn.hysds.net:8878"
-
-def is_authorized_ip(ip):
-    ips = set()
-    for x in authorized_ips.strip().split("\n"):
-        ips.add(x.split('#')[0].strip())
-
-    return ip in ips
 
 def submit_job(job_type, release, product_id, tag, job_params):
     '''
@@ -40,16 +33,16 @@ def submit_job(job_type, release, product_id, tag, job_params):
         'enable_dedup': False
 
     }
-    print('submitting jobs with params:')
-    print(json.dumps(params, sort_keys=True,indent=4, separators=(',', ': ')))
-    r = requests.post(job_submit_url, params=params)
-    if r.status_code != 200:
-        r.raise_for_status()
-    result = r.json()
+    print 'submitting jobs with params:'
+    print json.dumps(params, sort_keys=True, indent=4, separators=(',', ': '))
+    req = requests.post(job_submit_url, params=params)
+    if req.status_code != 200:
+        req.raise_for_status()
+    result = req.json()
     if 'result' in result.keys() and 'success' in result.keys():
-        if result['success'] == True:
+        if result['success'] is True:
             job_id = result['result']
-            print 'submitted create_aoi:%s job: %s job_id: %s' % (release, job_id)
+            print 'submitted upate ES:%s job: %s job_id: %s' % (job_type, release, job_id)
         else:
             print 'job not submitted successfully: %s' % result
             raise Exception('job not submitted successfully: %s' % result)
@@ -57,16 +50,21 @@ def submit_job(job_type, release, product_id, tag, job_params):
         raise Exception('job not submitted successfully: %s' % result)
 
 def lambda_handler(event, context):
-    print("Got event of type: %s" % type(event))
-    print("Got event: %s" % json.dumps(event, indent=2))
+    '''
+    This lambda handler calls submit_job with the job type info
+    and product id from the sns message
+    '''
+
+    print "Got event of type: %s" % type(event)
+    print "Got event: %s" % json.dumps(event, indent=2)
+    print "Got context: %s"% context
 
     product = event['ProductName']
-    print("From SNS product key: " + product)
-    
+    print "From SNS product key: %s" % product
     #submit mozart jobs to update ES
     job_type = "job-cmr_ingest_update"
     job_release = "release-20171103"
-    job_params = json.dumps(event) #pass the whole SNS message
+    job_params = {'sns_message': json.dumps(event)} #pass the whole SNS message
     tag = "asf_delivered"
-    
+
     submit_job(job_type, job_release, product, tag, job_params)
--- update_es.py
+++ update_es.py
@@ -1,90 +1,96 @@
-import json, requests
+#!/usr/bin/env python
+
+'''
+This script is for stamping docs in ES associated with products
+delivered to ASF with the delivery time to ASF and ASF's ingestion time
+'''
+
 import logging
-import sys, os, datetime
-import hysds_commons.request_utils
-import hysds_commons.metadata_rest_utils
-import osaka.main
-from string import Template
+import os
+import json
+import requests
 from hysds.celery import app
 
 #Setup logger for this job here.  Should log to STDOUT or STDERR as this is a job
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("hysds")
+LOGGER = logging.getLogger("hysds")
 
 BASE_PATH = os.path.dirname(__file__)
 
-def getURLIndexTypeID(product_id):
-   '''
-   Returns the ES URL, index, doctype, and id of the given product
-   @param product_id - id of the product to search for 
-   '''
-   es_url = app.conf.GRQ_ES_URL
-   es_index = "grq"
-   _id = product_id
-   query = {
+def get_url_index_type_id(_id):
+    '''
+    Returns the ES URL, index, doctype, and id of the given product
+    @param product_id - id of the product to search for
+    '''
+    es_url = app.conf.GRQ_ES_URL
+    es_index = "grq"
+    #_id = product_id
+    query = {
         "query": {
             "bool": {
                 "must": [
-                    { "term": { "_id": _id  }  }
+                    {"term": {"_id": _id}}
                 ]
             }
         }
-   }
-   url = "%s/%s/_search/" % (es_url, es_index)
-   data=json.dumps(query,indent=2)
-   print("Posting ES search: {0} with {1}".format(url,data))
-   r = requests.post(url,data=data)
-   r.raise_for_status()
-   print("Got: {0}".format(r.json()))
-   result = r.json()
-   if len(result["hits"]["hits"]) == 0:
-       raise Exception("Product not found in ES index: {0}".format(product_id))
-   elif len(result["hits"]["hits"])  > 1:
-       raise Exception("Product found multiple times: {0}".format(product_id))
-   return (es_url,result["hits"]["hits"][0]["_index"],result["hits"]["hits"][0]["_type"],result["hits"]["hits"][0]["_id"])
+    }
+    url = "%s/%s/_search/" % (es_url, es_index)
+    data = json.dumps(query, indent=2)
+    print "Posting ES search: {0} with {1}".format(url, data)
+    req_result = requests.post(url, data=data)
+    req_result.raise_for_status()
+    print "Got: {0}".format(req_result.json())
+    result = req_result.json()
+    if len(result["hits"]["hits"]) == 0:
+        raise Exception("Product not found in ES index: {0}".format(_id))
+    elif len(result["hits"]["hits"]) > 1:
+        raise Exception("Product found multiple times: {0}".format(_id))
+    _index = result["hits"]["hits"][0]["_index"]
+    _type = result["hits"]["hits"][0]["_type"]
+    _id = result["hits"]["hits"][0]["_id"]
+    return (es_url, _index, _type, _id)
 
-def update_document(product_id, delivery_time, ingest_time):
-   '''
-   Update the ES document with new information
-   Note: borrowed from user_tags
-   @param product_id - id of product delivered to ASF 
-   @param delivery_time - delivery time to ASF to stamp to delivered product
-   '''
+def update_document(_id, asf_delivery_time, asf_ingest_time):
+    '''
+    Update the ES document with new information
+    Note: borrowed from user_tags
+    @param product_id - id of product delivered to ASF
+    @param delivery_time - delivery time to ASF to stamp to delivered product
+    '''
 
-   new_doc = {
-      "doc": {
-         "ASF_delivery_time": delivery_time,
-         "ASF_ingestion_time": ingestion_time
-         },
-      "doc_as_upsert": true
-      }
+    new_doc = {
+        "doc": {
+            "ASF_delivery_time": asf_delivery_time,
+            "ASF_ingestion_time": asf_ingest_time
+        },
+        "doc_as_upsert": True
+        }
 
-   #url = "{0}/{1}/{2}/{3}/_update".format(*getURLIndexTypeID(product_id))
-   url = "{0}/{1}/grq_v1.1.2_s1-ifg_test/{3}/_update".format(*getURLIndexTypeID(product_id))
-   print("Updating: {0} with {1}".format(url,new_doc))
-   r = requests.post(url, data=json.dumps(new_doc))
-   if r.raise_for_status() == None:
-      return True
-   else:
-      return False
+    #url = "{0}/{1}/{2}/{3}/_update".format(*get_url_index_type_id(product_id))
+    #just for testing purposes. Created grq_v1.1.2_s1-ifg_test index. It is a copy of the ifg index.
+    url = "{0}/{1}/grq_v1.1.2_s1-ifg_test/{3}/_update".format(*get_url_index_type_id(_id))
+    print "Updating: {0} with {1}".format(url, json.dumps(new_doc))
+    req_result = requests.post(url, data=json.dumps(new_doc))
+    if req_result.raise_for_status() is  None:
+        return True
+    else:
+        print req_result.raise_for_status()
+        return False
 
 
 if __name__ == "__main__":
-   '''
-   Main program to update ES
-   '''
+    #Main program to update ES
 
-   with open('_context.json') as f:
-      ctx = json.load(f)
+    with open('_context.json') as f:
+        CTX = json.load(f)
 
-   #get SNS message from ASF
-   sns = ctx['sns_message']
-   #get product id
-   status = sns['Status']
-   product_id = sns['ProductName']
-   if status == "success":
-      delivery_time = sns['DeliveryTime']
-      ingest_time = sns['IngestTime'] 
-      if(update_document(product_id, delivery_time, ingest_time)):
-          print "Successfully updated ES document"
-   
+    #get SNS message from ASF
+    SNS = CTX['sns_message']
+    #get product id
+    STATUS = SNS['Status']
+    PRODUCT_ID = SNS['ProductName']
+    if STATUS == "success":
+        DELIVERY_TIME = SNS['DeliveryTime']
+        INGEST_TIME = SNS['IngestTime']
+        if update_document(PRODUCT_ID, DELIVERY_TIME, INGEST_TIME):
+            print "Successfully updated ES document"
