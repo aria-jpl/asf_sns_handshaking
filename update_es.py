779330f4c753159adcf96925c88f393b7b873f6c
Malarout <namrata.malarout@jpl.nasa.gov>
2019-05-08 14:44:27 -0700

    malarout: added alias to dynamically created index

--- update_es.py
+++ update_es.py
@@ -153,5 +153,6 @@ if __name__ == "__main__":
 
     if not ES.indices.exists(index="{}-released".format(_index)):
         ES.indices.create(index="{}-released".format(_index))
+        ES.indices.put_alias("{}-released".format(_index), name="grq_standard_product")
 
     deliver_to_aria_products(product_id, index=_index, doc_type=_type)
